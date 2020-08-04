55b9913c10bf7489013c2e5bce7078f21927da6a
Timothy Elling <elling@gmail.com>
2020-08-04 16:17:30 +0000

    Basic slide building working.
    Ready to start porting slides in.

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 echo "Hello"
 
-mkdir html
-for F in lecture.* ; do
-    echo $F ${F%.md}
+mkdir -p html
+for F in lectures/*.md ; do
+    cat slide.head.html $F slide.foot.html > html/$(basename ${F%.md}).html
 done
 
 cd html ; python3 -m http.server 8000
